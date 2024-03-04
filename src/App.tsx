130f45737676810c5e63b35c46e9600cd04e9449
sponge <sponge@d8d.org>
2024-03-04 16:38:44 -0500

    use videojs, virtualised display, buncha changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import { onMount, type Component, createSignal, For, Show, createEffect } from 'solid-js';
+import { onMount, type Component, createSignal, For, Show, createEffect, onCleanup } from 'solid-js';
 import { SetStoreFunction, createStore } from 'solid-js/store';
 import type { JSX } from 'solid-js';
 import styles from './App.module.css';
+
+import { VirtualContainer } from '@minht11/solid-virtual-container';
+
+import videojs from 'video.js';
+import Player from 'video.js/dist/types/player';
+import 'video.js/dist/video-js.css';
+
+const [selectedVideo, setSelectedVideo] = createSignal<Video | null>(null);
 
 interface Video {
   date: Date;
@@ -47,12 +55,96 @@ function createFilterStore(): [State, SetStoreFunction<State>] {
   return [store, setStore];
 }
 
+const VideoPlayer: Component<{ id: string }> = props => {
+  const [embiggen, setEmbiggen] = createSignal(false);
+
+  const onDialogClick: JSX.EventHandler<HTMLDialogElement, Event> = (evt) => {
+    if (evt.target === evt.currentTarget) {
+      setSelectedVideo(null);
+    }
+  };
+
+  let vidEl: HTMLVideoElement;
+  let player: Player;
+
+  // FIXME: this all shouldn't be in onmount, the async stuff can be handled some other way i think
+  onMount(async () => {
+    // grab the metadata and then find the download url
+    const resp = await fetch(`https://archive.org/metadata/${props.id}`);
+    const json = await resp.json();
+    const video = json.files.find((item: { format: string; }) => item.format === 'MPEG4');
+    const thumb = json.files.find((item: { format: string; }) => item.format === 'Thumbnail');
+    const src = `https://archive.org/download/${props.id}/${video.name}`;
+
+    // initialize the video player
+    const videoJsOptions = {
+      autoplay: true,
+      controls: true,
+      // sources: [{src, type: 'video/mp4'}]
+    };
+
+    // initialize videojs, use loadmedia so we can specify metadta
+    player = videojs(vidEl, videoJsOptions);
+    player.loadMedia({
+      title: json.metadata.title,
+      description: json.metadata.description,
+      poster: `https://archive.org/download/${props.id}/${thumb.name}`,
+      src: [{src, type: 'video/mp4'}]
+    }, () => {
+      // set current playback progress for the video if it exists
+      const progress = window.localStorage.getItem(props.id);
+      if (progress) {
+        player.currentTime(progress);
+      }
+    });
+
+    // for some reason the title bar is shown even while the video is playing???
+    player.on('play', () => {
+      document.querySelector('.vjs-title-bar')?.classList.add('hidden');
+    });
+
+    player.on('pause', () => {
+      document.querySelector('.vjs-title-bar')?.classList.remove('hidden');
+    });
+
+    // during playback, update localstorage
+    player.on('timeupdate', () => {
+      window.localStorage.setItem(props.id, player.currentTime()!.toString());
+    });
+
+    // add the embiggen button to the player
+    const Button = videojs.getComponent('Button');
+    const embiggenButton = new Button(player, {
+      // @ts-expect-error // its fine, videojs types are broke
+      clickHandler: () => {
+        setEmbiggen(!embiggen());
+      }
+    });
+    embiggenButton.addClass('embiggen-button');
+
+    // @ts-expect-error // here too
+    player.controlBar.addChild(embiggenButton, {}, player.controlBar.children_.length - 1);
+  });
+
+  onCleanup(() => {
+    player.dispose();
+  });
+
+  return <dialog open onClick={onDialogClick} class={embiggen() ? styles['dialog-embiggen'] : ''} >
+    <article>
+      {/* <header class="embiggen-hide">{vid().title}</header>
+    <p class="embiggen-hide"><small>{vid().description}</small></p> */}
+      <video class="video-js vjs-fill" ref={vidEl!} />
+    </article>
+  </dialog>;
+};
+
 const App: Component = () => {
   const [videos, setVideos] = createSignal<Video[]>([]);
   const [shows, setShows] = createSignal<string[]>([]);
-  const [selectedVideo, setSelectedVideo] = createSignal<Video | null>(null);
-  const [embiggen, setEmbiggen] = createSignal(false);
   const [filterState, setFilterState] = createFilterStore();
+
+  let scrollTargetElement!: HTMLTableRowElement;
 
   onMount(async () => {
     const resp = await fetch('archive-org.json');
@@ -110,80 +202,52 @@ const App: Component = () => {
     return filteredVids;
   };
 
-  const onDialogClick: JSX.EventHandler<HTMLDialogElement, Event> = (evt) => {
-    if (evt.target === evt.currentTarget) {
-      setSelectedVideo(null);
-    }
-  };
-
   const onSortChange: JSX.EventHandler<HTMLSelectElement, Event> = (evt) => {
     setFilterState({ sort: evt.currentTarget.value });
   };
 
   return (
     <main class="container">
-      <table class="striped">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Title</th>
-            <th>Show</th>
-          </tr>
-          <tr>
-            <td>
-              <select onChange={onSortChange} value={filterState.sort}>
-                <option value="newest-first">Newest first</option>
-                <option value="oldest-first">Oldest first</option>
-              </select>
-            </td>
-            <td>
-              <input type="search" onInput={search} value={filterState.title}/>
-            </td>
-            <td>
-              <Show when={shows().length > 0}>
-                <select onChange={filterByShow} value={filterState.show}>
-                  <option value="">(none)</option>
-                  <For each={shows()}>{show =>
-                    <option value={show}>{show}</option>
-                  }</For>
-                </select>
-              </Show>
-            </td>
-          </tr>
-        </thead>
-        <tbody>
-          <For each={filteredVideos()}>{vid =>
-            <tr>
-              <td>{vid.date.toLocaleDateString()}</td>
-              <td>
-                <span class={styles.video} onClick={[selectVideo, vid]}>{vid.title}</span>
-                <div class={styles.desc}>
-                  <small>{vid.description}</small>
-                </div>
-                {/* <a href={`https://archive.org/details/${vid.identifier}`}>🔗</a> */}
-                {/* <Show when={vid.identifier === selectedVideo()}>
-                  <iframe src={`https://archive.org/embed/${vid.identifier}`} width="640" height="480" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>
-                </Show> */}
-              </td>
-              <td>{vid.subject}</td>
-            </tr>
-          }</For>
-        </tbody>
-      </table>
-
+      <header>
+        <select onChange={onSortChange} value={filterState.sort}>
+          <option value="newest-first">Show newest first</option>
+          <option value="oldest-first">Show oldest first</option>
+        </select>
+        <input type="search" onInput={search} value={filterState.title} />
+        <Show when={shows().length > 0}>
+          <select onChange={filterByShow} value={filterState.show}>
+            <option value="">(none)</option>
+            <For each={shows()}>{show =>
+              <option value={show}>{show}</option>
+            }</For>
+          </select>
+        </Show>
+      </header>
+      <div class="virtual-parent" ref={scrollTargetElement}>
+        <VirtualContainer
+          items={filteredVideos()}
+          scrollTarget={scrollTargetElement}
+          itemSize={{ height: 60 }}
+        >
+          {props => <div class='list-item' style={props.style}>
+            <div class='date'>
+              {props.item.date.toLocaleDateString()}
+            </div>
+            <div class='title'>
+              <span class={styles.video} onClick={[selectVideo, props.item]}>{props.item.title}</span>
+              <div class={styles.desc}>
+                <small>{props.item.description}</small>
+              </div>
+            </div>
+            <div class='subject'>
+              {props.item.subject}
+            </div>
+          </div>}
+        </VirtualContainer>
+      </div>
       <Show when={selectedVideo()}>{vid =>
-        <dialog open onClick={onDialogClick} class={embiggen() ? styles['dialog-embiggen'] : ''} >
-          <article>
-            <header class="embiggen-hide">{vid().title}</header>
-            <p class="embiggen-hide"><small>{vid().description}</small></p>
-            <p>
-              <iframe class={embiggen() ? styles['embed-embiggen'] : styles.embed} src={`https://archive.org/embed/${vid().identifier}`} frameBorder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen />
-            </p>
-            <button class="outline secondary" onClick={() => setEmbiggen(!embiggen()) }>{embiggen() ? 'Debiggen' : 'Embiggen'}</button>
-          </article>
-        </dialog>
+        <VideoPlayer id={vid().identifier} />
       }</Show>
-
     </main>
   );
 };
